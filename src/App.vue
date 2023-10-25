9fa8cfc947ddd21d588b14af192e6cf77cc04313
himansh <sai.himanshum011@gmail.com>
2023-10-25 16:55:51 +0530

    Implemented header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-import {  RouterView } from 'vue-router'
+import llamaLogo from './assets/logo.svg'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <v-app>
+    <v-toolbar class="navigation">
+      <v-toolbar-side-icon>
+        <v-img :src="llamaLogo" height="30px" width="40px"> </v-img>
+      </v-toolbar-side-icon>
+      <v-toolbar-title class="title d-flex justify-center mr-8">MyLlama</v-toolbar-title>
+    </v-toolbar>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
-
 </style>

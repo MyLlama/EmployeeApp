839c968a3628364eebcff9703f509844a713f459
Disha <dishatalreja1202@gmail.com>
2023-11-08 16:49:55 +0530

    implemented bottom sheet for checklist, made things to do dynamic, updated the home card

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,21 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 import router from './router/index.ts'
 import i18n from './i18n.ts'
+import { VBottomSheet } from 'vuetify/labs/VBottomSheet'
 
 // Since we will using global components everywhere, it is advisable to import them in main file
 // Global components
 import BaseButton from './components/BaseButton.vue'
 
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components,
+    VBottomSheet
+  },
   directives
 })
 const app = createApp(App)
